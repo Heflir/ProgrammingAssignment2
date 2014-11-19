88859581b7595a0aa051f98b8e5a07127d2d1520
Heflir <susanne1.uebler@biologie.uni-regensburg.de>
2014-11-19 15:39:05 +0100

    Changed ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,27 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+##first set value of matrix
+
+inv<-NULL
+set<-function(y){
+x<<-y
+inv<<-NULL
+}
+
+##then get value of matrix
+
+get<-function()x
+
+##set inverse of matrix
+
+setinverse<-function(inverse) inv<<-inverse
+getinverse<-function() inv
+
+##then get inverse of matrix
+
+list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+
 }
 
 
@@ -12,4 +33,24 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+##get inverse of matrix
+
+inv=x$getinv()
+
+##check if inverse of matrix has already been calculated
+
+if(!is.null(inv)){
+message("getting cached data")
+return(inv)
+}
+
+##if not, calculate the inverse
+
+data=x$get()
+inv=solve(data, ...)
+
+##set inverse of matrix
+
+x$setinv(inv)
+return(inv)
 }
